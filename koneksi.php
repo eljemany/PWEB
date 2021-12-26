54774f8bd4b0accfd2c170f8024ea607d3df750e
eljemany <62747745+eljemany@users.noreply.github.com>
2021-12-26 21:47:58 +0700

    prakitkum10
    praktikum 10

--- koneksi.php
+++ koneksi.php
@@ -3,7 +3,7 @@ $host="localhost";
 $username="root";
 $password="";
 $databasename="akademik";
-$con=mysqli_connect($host,$username,$password,$databasename);
+$con=@mysqli_connect($host,$username,$password,$databasename);
 if (!$con) {
  echo "Error: " . mysqli_connect_error();
 exit();
